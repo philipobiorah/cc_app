dbd4e47892f41db2d7a9937038cc14509edc5ad2
PHILIP OBIORAH <philip.obiorah@uniport.edu.ng>
2018-07-01 13:23:44 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
-
     import Converter from './converter.js';
     import lastQuery from './converter.js';
     import IDBManager from './idbController.js';
 
 
-    //Create an IDb manager object for index db transactions
+    //Create an IDb manager object for Indexdb transactions
     let idbMan = new IDBManager();
 
     const submitButton = document.getElementById("submit_button");
@@ -16,12 +15,10 @@
 
     //create a converter object
     let converter = new Converter(idbMan);
-
-
         
     // fetch conversion rates and cache
-    const currencyIds = ["AUD", "USD", "NGN", "EUR", "MUR", "MXN", "XCD"];
-    currencyIds.forEach(fromCurrency => {
+    const currencyIds = ["AUD", "USD", "NGN", "EUR", "MUR", "MXN", "XCD"]; // Due to the limit in the free API a few  country ids are cached for 
+    currencyIds.forEach(fromCurrency => {                                  // complete offline user experience. 
         currencyIds.forEach(toCurrency => {
             if (fromCurrency !== toCurrency) {
                 console.log(`${fromCurrency}_${toCurrency}`);
@@ -30,7 +27,7 @@
         });
     });
 
-    // //Listen to when the submit button is clicked
+    // //Convert button event listener
     submitButton.addEventListener("click", () => 
     {
         let amount = amountEntry.value;
@@ -61,39 +58,3 @@
             alert("Please enter Amount to be converted");
         }
     });
-
-
-
-    function convertCurrency(){
-        submitButton.addEventListener("click", () => 
-    {
-        let amount = amountEntry.value;
-
-        if(amount)
-        {        
-            let fromCurrencyKey = fromSelect.options[fromSelect.selectedIndex].value;
-            let toCurrencyKey = toSelect.options[toSelect.selectedIndex].value;
-
-            if(fromCurrencyKey == toCurrencyKey)
-            {
-                convertedValueEntry.value = amount;
-            }
-            else
-            {
-                converter.convertCurrency(amount, fromCurrencyKey, toCurrencyKey, (error, result) => 
-                {
-                    convertedValueEntry.value = result || "0";
-                    if(error)
-                    {
-                        alert("Please check your internt connection | Error:"+error);
-                    }
-                });
-            }
-        }
-        else
-        {
-            alert("please enter the amount which you wish to convert");
-        }
-    });
-
-    }
